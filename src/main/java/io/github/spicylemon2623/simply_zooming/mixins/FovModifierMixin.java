afb8e4b374f89ef181e9d5d0fbedfaa85e5ca90e
Spicylemon2623 <spicylemon2623@gmail.com>
2024-11-03 23:06:59 +0000

    Fix zoom while pressing c in chat

--- src/main/java/io/github/spicylemon2623/simply_zooming/mixins/FovModifierMixin.java
+++ src/main/java/io/github/spicylemon2623/simply_zooming/mixins/FovModifierMixin.java
@@ -2,7 +2,10 @@ package io.github.spicylemon2623.simply_zooming.mixins;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import finalforeach.cosmicreach.gamestates.ChatMenu;
+import finalforeach.cosmicreach.gamestates.GameState;
 import finalforeach.cosmicreach.gamestates.InGame;
+import finalforeach.cosmicreach.settings.Controls;
 import finalforeach.cosmicreach.settings.FloatSetting;
 import io.github.spicylemon2623.simply_zooming.SZConfig;
 import io.github.spicylemon2623.simply_zooming.SZoomControls;
@@ -13,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.At;
 public class FovModifierMixin {
     @WrapOperation(method = "update(F)V", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/settings/FloatSetting;getValue()F"))
     private float getFov(FloatSetting instance, Operation<Float> original) {
-        if (SZoomControls.zoomKeybind.isPressed())
+        if (SZoomControls.zoomKeybind.isPressed() && !(GameState.currentGameState instanceof ChatMenu))
             return SZConfig.INSTANCE.zoomFov.getRealValue();
         else
             return original.call(instance);
